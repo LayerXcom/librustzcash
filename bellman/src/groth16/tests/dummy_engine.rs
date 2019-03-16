27745facd475592559829aae27dc23226089efd1
Osuke Sudo <dish230@gmail.com>
2019-03-16 23:34:21 +0900

    Add read to dummyEngine

--- bellman/src/groth16/tests/dummy_engine.rs
+++ bellman/src/groth16/tests/dummy_engine.rs
@@ -27,6 +27,11 @@ impl RW for Fr {
     fn write<W: ::io::Write>(&self, writer: &mut W) -> ::io::Result<()> {
         unimplemented!();
     }
+
+    fn read<R: ::io::Read>(reader: &mut R) -> ::io::Result<Self> {
+        unimplemented!();
+    }
+
 }
 
 impl fmt::Display for Fr {
